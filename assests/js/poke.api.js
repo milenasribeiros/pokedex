8cd8cc0c974ef30470b89e36fda43550b10e1712
milenasribeiros <milenassribeiro@gmail.com>
2023-05-19 10:24:52 -0300

    Arquivos Iniciais

---
+++ assests/js/poke.api.js
@@ -0,0 +1,24 @@
+const pokeApi = {}
+
+pokeApi.getPokemons = (offset= 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+            .then((response) => response.json())
+            .then((jsonBody) => jsonBody.results)
+            .catch((error) => console.error(error))
+}
+
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4'),
+    fetch('https://pokeapi.co/api/v2/pokemon/5'),
+    fetch('https://pokeapi.co/api/v2/pokemon/6'),
+    fetch('https://pokeapi.co/api/v2/pokemon/7'),
+    fetch('https://pokeapi.co/api/v2/pokemon/8'),
+    fetch('https://pokeapi.co/api/v2/pokemon/9'),
+    fetch('https://pokeapi.co/api/v2/pokemon/10')
+]).then((results) => {
+    console.log(results)
+})
