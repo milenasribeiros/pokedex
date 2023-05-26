9fc20919cea53cb95647d016f0a17b8a2b15b749
milenasribeiros <milenassribeiro@gmail.com>
2023-05-25 21:51:45 -0300

    Alterações da pokedex

--- assests/js/poke.api.js
+++ assests/js/poke.api.js
@@ -1,24 +1,34 @@
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokemonsDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokemonsDetail.id
+    pokemon.name = pokemonsDetail.name
+
+    const types = pokemonsDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokemonsDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon)
+}
+
 pokeApi.getPokemons = (offset= 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
-            .then((response) => response.json())
-            .then((jsonBody) => jsonBody.results)
-            .catch((error) => console.error(error))
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+            .then((detailRequests) => Promise.all(detailRequests))
+            .then((pokemonsDetails) => pokemonsDetails)
 }
 
-Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4'),
-    fetch('https://pokeapi.co/api/v2/pokemon/5'),
-    fetch('https://pokeapi.co/api/v2/pokemon/6'),
-    fetch('https://pokeapi.co/api/v2/pokemon/7'),
-    fetch('https://pokeapi.co/api/v2/pokemon/8'),
-    fetch('https://pokeapi.co/api/v2/pokemon/9'),
-    fetch('https://pokeapi.co/api/v2/pokemon/10')
-]).then((results) => {
-    console.log(results)
-})
